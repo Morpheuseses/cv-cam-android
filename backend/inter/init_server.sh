07eaf98f0743050917ba369873175a29b512e75e
morpheuseses <matvey.igoshev@yandex.ru>
2025-11-11 14:45:33 +0500

    Added init script for an inter server

---
+++ backend/inter/init_server.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo 'System Update';
+apt update && apt upgrade -y 
+
+apt install python3-venv
+
+mkdir server
+cd server
+
+echo 'Python venv initialization';
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+echo 'Starting server...'
+# 'python3 start_server.py &' or some other file
+status=$?
+
+if [ $status -ne 0 ]; then
+    echo "Error occurred while starting the server (код $status)"
+    exit $status
+else
+    echo "Server has been successfully started"
+fi
