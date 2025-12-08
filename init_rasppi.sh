44f691092e342c60fdd2921554a15f4330308d87
morpheuseses <matvey.igoshev@yandex.ru>
2025-12-08 19:35:44 +0500

    Подправил скрипт

--- init_rasppi.sh
+++ init_rasppi.sh
@@ -5,12 +5,17 @@ apt update -y && apt upgrade -y
 
 apt install -y python python3-venv
 
-echo 'Python venv initialization';
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
-mkdir -p ~/tmp
-TMPDIR=~/tmp pip install --no-cache-dir ultralytics
+if [ ! -d ".venv" ]; then
+  echo 'Python venv initialization';
+  python3 -m venv .venv
+  source .venv/bin/activate
+  pip install -r requirements.txt
+  mkdir -p ~/tmp
+  TMPDIR=~/tmp pip install --no-cache-dir ultralytics
+else 
+  echo 'Python venv already exists';
+  source .venv/bin/activate
+fi
 
 echo 'Starting client...'
 python3 rasppi.py 
