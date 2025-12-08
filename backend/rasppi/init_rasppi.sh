f1f8ee86575eca5b91720a6779c40ebdd6bdba44
morpheuseses <matvey.igoshev@yandex.ru>
2025-12-08 19:04:32 +0500

    Изменил bash-скрипты запуска

--- backend/rasppi/init_rasppi.sh
+++ backend/rasppi/init_rasppi.sh
@@ -3,13 +3,15 @@
 echo 'System Update';
 apt update -y && apt upgrade -y 
 
-apt install -y python3-venv
+apt install -y python python3-venv
 
 echo 'Python venv initialization';
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
+mkdir -p ~/tmp
+TMPDIR=~/tmp pip install --no-cache-dir ultralytics
 
-echo 'Starting server...'
-python3 start_server.py
+echo 'Starting client...'
+python3 rasppi.py 
 
